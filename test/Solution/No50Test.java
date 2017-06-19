36497029a93bb9f20ed33fbb13f9df1c3543e95c
Caily Wu <hsqsa@hotmail.com>
2017-06-19 21:43:08 +0800

    add No.50

---
+++ test/Solution/No50Test.java
@@ -0,0 +1,18 @@
+package Solution;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Caily on 6/19/2017.
+ */
+public class No50Test {
+    private No50 test= new No50();
+
+    @Test
+    public void myPow() throws Exception {
+        test.myPow(8.88023,5);
+    }
+
+}
