c4313a25c9d84dbf980a68f254b4d31fa04494aa
caily <hsqsa@hotmail.com>
2017-06-26 11:03:52 +0800

    add No.74&75

---
+++ test/Solution/No75Test.java
@@ -0,0 +1,17 @@
+package Solution;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by caily on 6/26/17.
+ */
+public class No75Test {
+    private No75 test = new No75();
+    @Test
+    public void sortColors() throws Exception {
+        test.sortColors(new int[]{0});
+    }
+
+}
